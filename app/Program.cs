6477396eda7a7fb854b5a0e4af515a86605fb183
Johan Hellgren <johan.hellgren@slu.se>
2023-09-13 14:19:19 +0200

    added some more logging examples

--- app/Program.cs
+++ app/Program.cs
@@ -13,6 +13,12 @@ while (await timer.WaitForNextTickAsync())
     var position = new { Latitude = 25, Longitude = 134 };
     var elapsedMs = 34;
     log.Information("Processed {@Position} in {Elapsed} ms", position, elapsedMs);
+
+    var context = new { Id = 1, Name = "Foo" };
+    log.Warning("Warning warning, please see context {@Context}", context);
+
+    var exception = new Exception("Boom!");
+    log.Error(exception, "Exception thrown");
 }
 
 
