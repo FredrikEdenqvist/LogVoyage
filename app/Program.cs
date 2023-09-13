0ea0671353aec32f1802e09774668e8b0e997c33
Johan Hellgren <johan.hellgren@slu.se>
2023-09-13 11:17:47 +0200

    cleaned up demo

--- app/Program.cs
+++ app/Program.cs
@@ -9,7 +9,7 @@ var log = new LoggerConfiguration()
 var timer = new PeriodicTimer(TimeSpan.FromSeconds(10));
 while (await timer.WaitForNextTickAsync())
 {
-    log.Information("Hello, Serilog this entry has no data!");
+    log.Information("Hello, Serilog! this entry has no data.");
     var position = new { Latitude = 25, Longitude = 134 };
     var elapsedMs = 34;
     log.Information("Processed {@Position} in {Elapsed} ms", position, elapsedMs);
